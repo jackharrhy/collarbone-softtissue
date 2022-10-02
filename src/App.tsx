4cb07bde5b8c7838256ed289d709475236638401
Jack Arthur Harrhy <me@jackharrhy.com>
2022-10-01 22:24:43 -0230

    pages time
    this will break due to path issues, fixing in next commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { Link, Outlet } from "react-router-dom";
 
+// <Link to="/collarbone-softtissue">Collarbone Softtissue</Link>
+
 export const App = () => (
   <>
     <nav>
-      <Link to="/collarbone-softtissue">Collarbone Softtissue</Link>
       <Link to="/mitchell-hynes-student">Mitchell Hynes Student</Link>
       <Link to="/stare">Stare</Link>
     </nav>
